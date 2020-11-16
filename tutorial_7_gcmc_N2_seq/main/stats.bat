66ba44aac2da5ce1bc2daa0d1677dc7fd5330988
by-student-2017 <studentsctest@gmail.com>
2020-11-16 16:57:58 +0900

    windows10

---
+++ tutorial_7_gcmc_N2_seq/main/stats.bat
@@ -0,0 +1,11 @@
+for /l %%i in (90000,100,100000) do (
+  find " %%i " <log.lammps>> data.txt
+)
+
+gnuplot stats.gpl > stats.txt
+
+echo                        MPa                 wt.%% > meam_and_std.txt
+find " Mean: " <stats.txt>> meam_and_std.txt
+find " Std Dev: " <stats.txt>> meam_and_std.txt
+
+del data.txt stats.txt
