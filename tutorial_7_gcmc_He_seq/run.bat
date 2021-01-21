d4c0d7d6e8c155f540e791d488abbd7d7b855748
by-student-2017 <studentsctest@gmail.com>
2021-01-21 09:48:05 +0900

    windows10

--- tutorial_7_gcmc_He_seq/run.bat
+++ tutorial_7_gcmc_He_seq/run.bat
@@ -1,5 +1,8 @@
 echo # type                 MPa                 wt.%% > info.txt
 
+rem "GCMC calculation time, ps"
+set s=600
+
 rem "temperature, K"
 set t=273.15
 
@@ -11,8 +14,9 @@ for %%i in (2.5 5.0 15.0 30.0 50.0 70.0 100.0) do (
   cd %%i
   mkdir cfg
   echo clear > in.lmp1
-  echo variable   temp1 index %t% >> in.lmp1
-  echo variable   mpa   index %%i >> in.lmp1
+  echo variable   temp1  index %t% >> in.lmp1
+  echo variable   mpa    index %%i >> in.lmp1
+  echo variable   tgcmc1 index %s% >> in.lmp1
   copy /b in.lmp1 + in.lmp2 in.lmp
   copy /b in.lmp1 + in.lmp_restart2 in.lmp_restart
   "C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.lmp
--- tutorial_7_gcmc_He_seq/run_seq_tps.bat
+++ tutorial_7_gcmc_He_seq/run_seq_tps.bat
@@ -1,5 +1,8 @@
 echo # type                 MPa                 wt.%% > info.txt
 
+rem "GCMC calculation time, ps"
+set s=600
+
 rem "temperature, K"
 for %%t in (233.15 248.15 273.15 298.15 313.15 358.15) do (
   mkdir %%t
@@ -11,8 +14,9 @@ for %%t in (233.15 248.15 273.15 298.15 313.15 358.15) do (
     cd %%i
     mkdir cfg
     echo clear > in.lmp1
-    echo variable   temp1 index %%t >> in.lmp1
-    echo variable   mpa   index %%i >> in.lmp1
+    echo variable   temp1  index %%t >> in.lmp1
+    echo variable   mpa    index %%i >> in.lmp1
+    echo variable   tgcmc1 index %s% >> in.lmp1
     copy /b in.lmp1 + in.lmp2 in.lmp
     copy /b in.lmp1 + in.lmp_restart2 in.lmp_restart
     "C:\Program Files\LAMMPS 64-bit 18Jun2019\bin\lmp_serial.exe" -in in.lmp
