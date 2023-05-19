d0caf9f0ce4d801354598868e61a71394c0efffb
by-student-2017 <studentsctest@gmail.com>
2023-05-19 13:26:04 +0900

    windows11

--- tutorial_7_gcmc_H2_seq/main/stats.bat
+++ tutorial_7_gcmc_H2_seq/main/stats.bat
@@ -2,7 +2,8 @@ for /l %%i in (400000,4000,1600000) do (
   find " %%i " <log.lammps>> data.txt
 )
 
-gnuplot stats.gpl > stats.txt
+rem "gnuplot stats.gpl > stats.txt"
+"C:\Program Files\gnuplot\bin\gnuplot.exe" stats.gpl > stats.txt"
 
 echo #                      MPa                 wt.%% > meam_and_std.txt
 find " Mean: " <stats.txt>> meam_and_std.txt
---
+++ tutorial_7_gcmc_H2_seq/run_msmpi.bat
@@ -0,0 +1,37 @@
+echo # type                 MPa                 wt.%% > info.txt
+
+rem "GCMC calculation time, ps"
+set s=600
+
+rem "Number of CPU"
+set ncore=8
+
+rem "temperature, K"
+rem "set t=273.15"
+set t=423.15
+
+rem "pressure, MPa"
+rem "for %%i in (2.5 5.0 15.0 30.0 50.0 70.0 100.0) do ("
+for %%i in (70.0 30.0 15.0) do (
+  mkdir %%i
+  REM xcopy /y main %%i
+  copy .\main\* %%i 
+  cd %%i
+  mkdir cfg
+  echo clear > in.lmp1
+  echo variable   temp1  index %t% >> in.lmp1
+  echo variable   mpa    index %%i >> in.lmp1
+  echo variable   tgcmc1 index %s% >> in.lmp1
+  copy /b in.lmp1 + in.lmp2 in.lmp
+  copy /b in.lmp1 + in.lmp_restart2 in.lmp_restart
+
+  mpiexec.exe -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in in.lmp
+
+  del in.lmp1 in.lmp2 in.lmp_restart2
+  call stats.bat
+  find " Mean: " <meam_and_std.txt>> ..\info.txt
+  find " Std Dev: " <meam_and_std.txt>> ..\info.txt
+  cd ..
+)
+
+gnuplot plot_p_vs_wt.gpl
