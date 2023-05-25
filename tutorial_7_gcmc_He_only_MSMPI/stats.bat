399362469d62c57237af1f816ecdbc7af64a4666
by-student-2017 <studentsctest@gmail.com>
2023-05-25 11:39:04 +0900

    windows11

---
+++ tutorial_7_gcmc_He_only_MSMPI/stats.bat
@@ -0,0 +1,11 @@
+for /l %%i in (99000,100,100000) do (
+  find " %%i " <log.lammps>> data.txt
+)
+
+"C:\Program Files\gnuplot\bin\gnuplot.exe" stats.gpl > stats.txt
+
+echo #                      MPa                 He > meam_and_std.txt
+find " Mean: " <stats.txt>> meam_and_std.txt
+find " Std Dev: " <stats.txt>> meam_and_std.txt
+
+del data.txt stats.txt
