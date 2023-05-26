e16d44c016b6bf83afe506179a4d77a74951e2c0
by-student-2017 <studentsctest@gmail.com>
2023-05-26 10:23:01 +0900

    windows11

---
+++ run_seq_H2_and_He.bat
@@ -0,0 +1,10 @@
+
+cd ./tutorial_7_gcmc_H2_seq_MSMPI
+call run_msmpi.bat
+cd ../
+
+
+cd ./tutorial_7_gcmc_He_seq_MSMPI
+call run_msmpi.bat
+cd ../
+
---
+++ run_seq_tps_H2_and_He.bat
@@ -0,0 +1,10 @@
+
+cd ./tutorial_7_gcmc_H2_seq_MSMPI
+call run_seq_tps_msmpi.bat
+cd ../
+
+
+cd ./tutorial_7_gcmc_He_seq_MSMPI
+call run_seq_tps_msmpi.bat
+cd ../
+
