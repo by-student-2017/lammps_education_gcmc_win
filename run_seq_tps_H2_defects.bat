e1788952f4336b17a9557410b7fddd47d3594a85
by-student-2017 <studentsctest@gmail.com>
2024-01-19 17:53:52 +0900

    WSL2(ubuntu 22.04 LTS) on win11

--- run_seq_tps_H2_defects.bat
+++ run_seq_tps_H2_defects.bat
@@ -1,19 +1,14 @@
 
 cd ./tutorial_7_gcmc_H2_seq_MSMPI_defectB
-call run_seq_tps_msmpi.bat
+call run_seq_tps_msmpi_01fs.bat
 cd ../
 
 
 cd ./tutorial_7_gcmc_H2_seq_MSMPI_defectN
-call run_seq_tps_msmpi.bat
+call run_seq_tps_msmpi_01fs.bat
 cd ../
 
 
 cd ./tutorial_7_gcmc_H2_seq_MSMPI_defectNB
-call run_seq_tps_msmpi.bat
-cd ../
-
-
-cd ./tutorial_7_gcmc_H2_seq_MSMPI
-call run_seq_tps_msmpi.bat
+call run_seq_tps_msmpi_01fs.bat
 cd ../
